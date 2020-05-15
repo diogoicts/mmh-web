e609614ad535ba52234ba2730b0cf53be55d535e
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2020-05-15 18:37:47 -0400

    reset password

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -22,7 +22,7 @@ const Menu = () => {
 
   async function handleLogout (e) {
     try { 
-      const response = await api.post("/auth/logout");
+      await api.post("/auth/logout");
       logout();
       history.push("/");
     } catch (err) {
