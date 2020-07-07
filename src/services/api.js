55d693106a2a2429fb040f60b0a12aa36e2d5472
Bruno Campos <brunocampos@meliuz.com.br>
2020-07-07 01:23:02 -0400

    Adicionar suporte para usar outras portas
    A porta 80 é a porta padrão usada no projeto. Porém, em alguns
    ambientes ela já é usada por outras aplicações. Por isso, este
    commit adiciona uma forma de usar outras variáveis de ambiente.
    
    Além disso, criamos mais uma variável de ambiente para definir o
    domínio da aplicação. Com isso, vamos conseguir montar corretamente
    a URL para acessar a API no Backend.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
+const port = process.env.REACT_APP_BACKEND_PORT;
+const domain = process.env.REACT_APP_DOMAIN;
+const baseUrl = `http://back.${domain}`;
+const apiUrl = (port != null && port != 80) ? `${baseUrl}:${port}/api/v1` : `${baseUrl}/api/v1`;
+
 const api = axios.create({
-  baseURL: 'http://back.localhost/api/v1',
+  baseURL: apiUrl,
 })
 
 api.interceptors.request.use(async config => {
